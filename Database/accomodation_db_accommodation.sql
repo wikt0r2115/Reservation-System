bc173d0768d0c429c74fa1b020269817f944a30f
wikt0r2115 <wiktorw2134@gmail.com>
2025-12-01 12:11:17 +0100

    Added export bazy danych my sql

---
+++ Database/accomodation_db_accommodation.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+--
+-- Host: localhost    Database: accomodation_db
+-- ------------------------------------------------------
+-- Server version	8.0.44
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `accommodation`
+--
+
+DROP TABLE IF EXISTS `accommodation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `accommodation` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `beds` int NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `location_name` varchar(255) NOT NULL,
+  `price` decimal(38,2) NOT NULL,
+  `rooms` int NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `accommodation_chk_1` CHECK ((`beds` >= 1)),
+  CONSTRAINT `accommodation_chk_2` CHECK ((`rooms` >= 1))
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `accommodation`
+--
+
+LOCK TABLES `accommodation` WRITE;
+/*!40000 ALTER TABLE `accommodation` DISABLE KEYS */;
+INSERT INTO `accommodation` VALUES (1,4,'Testowe mieszkanie przy Motlawie','Gdansk Stare Miasto',350.00,2),(2,2,'test','test',2.00,2),(3,2,'test2','test2',2.00,2),(4,2,'2','test',2.00,2);
+/*!40000 ALTER TABLE `accommodation` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-12-01 12:09:17
